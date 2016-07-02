7e84c446e5b98e8a5337e084f414a3e7ddffbbb4
Joel James <joel.james@localmed.com>
2016-07-02 09:40:16 -0500

    Add missing utils test

--- tests/unit_tests/pyserializer/utils_tests.py
+++ tests/unit_tests/pyserializer/utils_tests.py
@@ -32,3 +32,28 @@ class TestUtil:
     def test_is_terable_false(self):
         output = is_iterable(1)
         assert_false(output)
+
+    def test_force_str(self):
+        output = force_str(b'some_value')
+        assert_equal(output, 'some_value')
+
+    def test_force_str_with_str(self):
+        output = force_str('some_value')
+        assert_equal(output, 'some_value')
+
+    def test_get_object_by_source(self):
+        user = Mock(name='user', username='foo.bar.com')
+        output = get_object_by_source(
+            user,
+            source='username'
+        )
+        assert_equal(output, 'foo.bar.com')
+
+    def test_get_object_by_source_with_dot_syntax(self):
+        user = Mock(name='user', username='foo.bar.com')
+        comment = Mock(name='comment', user=user)
+        output = get_object_by_source(
+            comment,
+            source='user.username'
+        )
+        assert_equal(output, 'foo.bar.com')
