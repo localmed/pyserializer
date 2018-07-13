b8ead29531710226c4ee2a9b7e26d88cd5107c9e
Pete Browne <pete.browne@localmed.com>
2018-07-13 10:43:46 -0500

    Relax six dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     install_requires=[
-        'six==1.10.0'
+        'six>=1.10.0'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
